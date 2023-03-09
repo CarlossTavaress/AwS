760a00239f3f6609c3e3c156944eed25203b6cb4
Carlos Tavares <carlosmatheustavares@gmail.com>
2023-03-09 17:57:41 -0300

    inversosrdeString

---
+++ soma.js
@@ -0,0 +1,16 @@
+function inverterString(str) {
+  return str.split('').reverse().join('');
+}
+function inverterString(str) {
+  let novaString = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    novaString += str[i];
+  }
+  return novaString;
+}
+function inverterString(str) {
+  return str.split('').reduce((acc, char) => char + acc, '');
+}
+const minhaString = 'Exemplo de string para inverter';
+const minhaStringInvertida = inverterString(minhaString);
+console.log(minhaStringInvertida); 
